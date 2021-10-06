7ccd19a1ea4359aeed424bdf56ea201f0755fd4a
Christian Provenzano <18606244+caproven@users.noreply.github.com>
2021-10-05 22:59:10 -0400

    chore: initial operator-sdk scaffolding

---
+++ api/v1/kappa_types.go
@@ -0,0 +1,64 @@
+/*
+Copyright 2021 caproven.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package v1
+
+import (
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+// EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
+// NOTE: json tags are required.  Any new fields you add must have json tags for the fields to be serialized.
+
+// KappaSpec defines the desired state of Kappa
+type KappaSpec struct {
+	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
+	// Important: Run "make" to regenerate code after modifying this file
+
+	// Foo is an example field of Kappa. Edit kappa_types.go to remove/update
+	Foo string `json:"foo,omitempty"`
+}
+
+// KappaStatus defines the observed state of Kappa
+type KappaStatus struct {
+	// INSERT ADDITIONAL STATUS FIELD - define observed state of cluster
+	// Important: Run "make" to regenerate code after modifying this file
+}
+
+//+kubebuilder:object:root=true
+//+kubebuilder:subresource:status
+
+// Kappa is the Schema for the kappas API
+type Kappa struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+
+	Spec   KappaSpec   `json:"spec,omitempty"`
+	Status KappaStatus `json:"status,omitempty"`
+}
+
+//+kubebuilder:object:root=true
+
+// KappaList contains a list of Kappa
+type KappaList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata,omitempty"`
+	Items           []Kappa `json:"items"`
+}
+
+func init() {
+	SchemeBuilder.Register(&Kappa{}, &KappaList{})
+}
